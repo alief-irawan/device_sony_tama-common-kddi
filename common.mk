36a14dfcbd538184e48c9919a3b6b575af8597f3
alief irawan <aliefirawan2@gmail.com>
2022-04-10 23:43:30 +0000

    tama-common-kddi: Clean up

--- common.mk
+++ common.mk
@@ -12,7 +12,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
 # Get non-open-source specific aspects
-$(call inherit-product-if-exists, vendor/sony/tama-common/tama-common-vendor.mk)
+$(call inherit-product-if-exists, vendor/sony/tama-common-kddi/tama-common-kddi-vendor.mk)
 
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
